f4aad40689c4581222d344256b5853f32aab7256
Abdelhaq laachari <93976987+abdelhaq-laachari@users.noreply.github.com>
2022-11-21 15:05:46 +0100

    create an error handler

--- code/server/api/controller/adminController.js
+++ code/server/api/controller/adminController.js
@@ -5,7 +5,6 @@ const asyncHandler = require("express-async-handler");
 // @access  Public
 
 const signIn = asyncHandler(async (req, res) => {
-  //   res.status(200).json({ message: "user has logged in" });
   const {firstName, lastName} = req.body;
   if (!firstName || !lastName) {
     res.status(400);
