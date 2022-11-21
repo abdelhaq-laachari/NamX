6991b3647bdd0caae06b9dedcb11ed0d1c9ab89f
Abdelhaq laachari <93976987+abdelhaq-laachari@users.noreply.github.com>
2022-11-21 14:45:53 +0100

    create an error handler

--- code/server/api/controller/adminController.js
+++ code/server/api/controller/adminController.js
@@ -1,6 +1,18 @@
-const signIn = (req, res) => {
-  res.status(200).json({ message: "user has logged in" });
-};
+const asyncHandler = require("express-async-handler");
+
+// @desc    Sign in a new admin
+// @route   POST /admin/signIn
+// @access  Public
+
+const signIn = asyncHandler(async (req, res) => {
+  //   res.status(200).json({ message: "user has logged in" });
+  const {firstName, lastName} = req.body;
+  if (!firstName || !lastName) {
+    res.status(400);
+    throw new Error("Please fill in all fields");
+  }
+  res.status(201).json(firstName);
+});
 
 module.exports = {
   signIn,
