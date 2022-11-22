0ef404728f542510c3252d4d58b014cf28d2d78f
zinebaboumejd <zinebaboumejd@gmail.com>
2022-11-22 19:49:01 +0100

    add clientSchema

---
+++ code/server/api/models/ClientModel.js
@@ -0,0 +1,36 @@
+const mongoose = require("mongoose");
+
+const clientSchema=mongoose.Schema({
+    firstName:{
+        type:String,
+        required:[true,"Please enter your first name"]
+    } ,
+    lastName:{
+        type:String,
+        required:[true,"Please enter your last name"]
+    } ,
+    Address:{
+        type:String,
+        required:[true,"Please enter your Address"]
+    },
+    City:{
+        type:String,
+        required:[true,"Please enter your City"]
+    },
+    phoneNumber:{
+        type:String,
+        required:[true,"Please enter your phoneNumber"]
+    },
+    email:{
+        type:String,
+        required:[true,"Please enter your email"],
+        unique:true
+    },
+    password:{
+        type:String,
+        required:[true,"Please enter your password"]
+    }
+
+})
+
+module.exports=mongoose.model("Client",clientSchema)
