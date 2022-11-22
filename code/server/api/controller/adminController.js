f14fac5a0adfd5b08098af7abd186d56833e772c
Abdelhaq laachari <93976987+abdelhaq-laachari@users.noreply.github.com>
2022-11-22 21:38:05 +0100

    i just create a function for admin to update his information

--- code/server/api/controller/adminController.js
+++ code/server/api/controller/adminController.js
@@ -5,10 +5,22 @@ const Admin = require("../models/adminModel");
 // @route   POST /admin/signIn
 // @access  Public
 
-const signIn = asyncHandler(async (req, res) => {
+const signIn = asyncHandler(async (req, res) => {});
 
+const updateAdmin = asyncHandler(async (req, res) => {
+  const adminId = req.params.id;
+  const admin = await Admin.findById(adminId);
+  if (!admin) {
+    res.status(404);
+    throw new Error("Admin not found");
+  }
+  const updateProfile = await Admin.findByIdAndUpdate(req.admin.id, req.body, {
+    new: true,
+  });
+  res.status(200).json(updateProfile);
 });
 
 module.exports = {
   signIn,
+  updateAdmin,
 };
--- code/server/api/routes/adminRoutes.js
+++ code/server/api/routes/adminRoutes.js
@@ -1,11 +1,8 @@
 const express = require("express");
 const router = express.Router();
-const {signIn} = require("../controller/adminController")
-
-
+const { signIn, updateAdmin } = require("../controller/adminController");
 
 router.route("/signIn").post(signIn);
-
-
+router.route("/update/:id").put(updateAdmin);
 
 module.exports = router;
