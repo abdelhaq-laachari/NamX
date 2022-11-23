b28e8937d5cea7da754d40e3d4aa443ed32cf6c0
Abdelhaq laachari <93976987+abdelhaq-laachari@users.noreply.github.com>
2022-11-23 17:06:41 +0100

    create function for add details

--- code/server/api/controller/detailsController.js
+++ code/server/api/controller/detailsController.js
@@ -2,15 +2,15 @@ const asyncHandler = require("express-async-handler");
 const Details = require("../models/detailsModel");
 
 // @desc    Get all Details
-// @route   GET /api/details
+// @route   GET admin/details
 // @access  Private
 const getDetails = asyncHandler(async (req, res) => {
   const details = await Details.find();
   res.status(200).json(details);
 });
 
-// @desc    Add a new car
-// @route   POST /api/cars
+// @desc    Add a new details
+// @route   POST admin/details
 // @access  Private
 const addDetails = asyncHandler(async (req, res) => {
   const { libelle, value, car } = req.body;
