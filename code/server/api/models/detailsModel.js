308672995f68c8dd53c00836e8fb84fef04019a4
Abdelhaq laachari <93976987+abdelhaq-laachari@users.noreply.github.com>
2022-11-24 09:57:17 +0100

    update schema

--- code/server/api/models/detailsModel.js
+++ code/server/api/models/detailsModel.js
@@ -1,16 +1,21 @@
 const mongoose = require("mongoose");
-const detailsSchema = mongoose.Schema({
-  libelle: {
-    type: String,
-    required: [true, "Please enter your libelle"],
+const detailsSchema = mongoose.Schema(
+  {
+    libelle: {
+      type: String,
+      required: [true, "Please enter your libelle"],
+    },
+    value: {
+      type: String,
+      required: [true, "Please enter your value"],
+    },
+    car: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Car",
+    },
   },
-  value: {
-    type: String,
-    required: [true, "Please enter your value"],
-  },
-  car: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "Car",
-  },
-});
+  {
+    timestamp: true,
+  }
+);
 module.exports = mongoose.model("Details", detailsSchema);
