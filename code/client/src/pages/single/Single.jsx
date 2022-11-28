51e1673cc9eca5023bdaa40cc8af289d221e3526
Abdelhaq laachari <93976987+abdelhaq-laachari@users.noreply.github.com>
2022-11-28 14:02:39 +0100

    working on profile page

--- code/client/src/pages/single/Single.jsx
+++ code/client/src/pages/single/Single.jsx
@@ -1,8 +1,6 @@
 import "./single.scss";
 import Sidebar from "../../components/sidebar/Sidebar";
 import Navbar from "../../components/navbar/Navbar";
-import Chart from "../../components/chart/Chart";
-import List from "../../components/table/Table";
 
 const Single = () => {
   return (
@@ -43,13 +41,6 @@ const Single = () => {
               </div>
             </div>
           </div>
-          <div className="right">
-            <Chart aspect={3 / 1} title="User Spending ( Last 6 Months)" />
-          </div>
-        </div>
-        <div className="bottom">
-        <h1 className="title">Last Transactions</h1>
-          <List/>
         </div>
       </div>
     </div>
