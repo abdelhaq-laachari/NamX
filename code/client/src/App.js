cb1c3491bf720fa948bbb839b1a03a33a7796e84
Abdelhaq laachari <93976987+abdelhaq-laachari@users.noreply.github.com>
2022-11-28 10:10:37 +0100

    i just finished two pages that wi will use in authentication section

--- code/client/src/App.js
+++ code/client/src/App.js
@@ -1,5 +1,6 @@
 import Home from "./pages/home/Home";
 import Login from "./pages/login/Login";
+import Register from "./pages/register/Register";
 import List from "./pages/list/List";
 import Single from "./pages/single/Single";
 import New from "./pages/new/New";
@@ -19,6 +20,7 @@ function App() {
           <Route path="/">
             <Route index element={<Home />} />
             <Route path="login" element={<Login />} />
+            <Route path="register" element={<Register />} />
             <Route path="users">
               <Route index element={<List />} />
               <Route path=":userId" element={<Single />} />
