12797963a71ff8c2e03c746605e48b8299dd2562
Abdelhaq laachari <93976987+abdelhaq-laachari@users.noreply.github.com>
2022-12-07 09:18:14 +0100

    workking on profile

--- code/client/src/components/single/Single.jsx
+++ code/client/src/components/single/Single.jsx
@@ -6,12 +6,15 @@ import New from "../../pages/new/New";
 import { userInputs } from "../../formSource";
 
 const Single = ({ data }) => {
+  const path = window.location.pathname.split("/")[1];
+  console.log(path);
   return (
     <div className="single">
       <Sidebar />
       <div className="singleContainer">
         <Navbar />
-        <div className="top">
+        {path === "user" ? (
+          <div className="top">
           <div className="left">
             <div className="editButton">
               <Link
@@ -33,7 +36,7 @@ const Single = ({ data }) => {
                 <h1 className="itemTitle">{data?.firstName} {data?.lastName}</h1>
                 <div className="detailItem"> 
                   <span className="itemKey">Email:</span>
-                  <span className="itemValue">{data.email}</span>
+                  <span className="itemValue">{data?.email}</span>
                 </div>
                 <div className="detailItem">
                   <span className="itemKey">Phone:</span>
@@ -57,6 +60,44 @@ const Single = ({ data }) => {
             </div>
           </div>
         </div>
+        ) : (
+          <div className="top">
+          <div className="left">
+            <h1 className="title">Information</h1>
+            <div className="item">
+              <img
+                src="https://images.pexels.com/photos/733872/pexels-photo-733872.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=750&w=1260"
+                alt=""
+                className="itemImg"
+              />
+              <div className="details">
+                <h1 className="itemTitle">{data?.firstName} {data?.lastName}</h1>
+                <div className="detailItem"> 
+                  <span className="itemKey">Email:</span>
+                  <span className="itemValue">{data?.email}</span>
+                </div>
+                <div className="detailItem">
+                  <span className="itemKey">Phone:</span>
+                  <span className="itemValue">+1 2345 67 89</span>
+                </div>
+                <div className="detailItem">
+                  <span className="itemKey">Address:</span>
+                  <span className="itemValue">
+                    Elton St. 234 Garden Yd. NewYork
+                  </span>
+                  {/* <span className="itemValue">
+                    {data.address}
+                  </span> */}
+                </div>
+                <div className="detailItem">
+                  <span className="itemKey">Country:</span>
+                  <span className="itemValue">Morocco</span>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        )}
       </div>
     </div>
   );
