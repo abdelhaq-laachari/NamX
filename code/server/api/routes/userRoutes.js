04b1d334e973e1f8da2c3f10be64b5461f1b8bdd
zinebaboumejd <zinebaboumejd@gmail.com>
2022-12-16 10:18:30 +0100

    add route getCar user

--- code/server/api/routes/userRoutes.js
+++ code/server/api/routes/userRoutes.js
@@ -1,5 +1,9 @@
 const express = require("express"); // Import express
 const router = express.Router(); // Create a router
 
+// get function from car controller 
+const { getCars} = require("../controller/carController");
 
+// add route for car crud
+router.route("/getCars").get(getCars);
 module.exports = router;
